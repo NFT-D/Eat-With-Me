fbfdffe21f68f521a6b9b88569ae1c930ff22d7c
Dean <deanju1102@gmail.com>
2022-12-29 13:07:37 -0500

    publish

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 export default {
   "expo": {
-    "owner": "nft-d",
+    "owner": "dj1102",
     "name": "Eat-With-Me",
     "slug": "Eat-With-Me",
     "version": "1.0.0",
@@ -23,6 +23,8 @@ export default {
       "supportsTablet": true
     },
     "android": {
+      "package": "com.nftd.eatwithme",
+      "versionCode": 1,
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#FFFFFF"
@@ -38,6 +40,9 @@ export default {
       firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       firebaseAppId: process.env.FIREBASE_APP_ID,
+      "eas": {
+        "projectId": "99ecb859-6f7e-4ebd-8d44-3ce016c3c4fe"
+      }
     }
   }
 }
